ffbfdbecd81fa646360e23f945900fb9453b15d9
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-06-09 01:53:22 +0100

    Finished the project

--- birthday-reminder/src/App.jsx
+++ birthday-reminder/src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
+import data from "./data";
+import List from "./components/List";
 
 const App = () => {
-  return <div>App</div>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
 };
 
 export default App;
