df6f12a9aa08ba2eed1cb96ef6312e47566d229d
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-06-19 11:45:52 +0100

    Added content to the SingleProduct page

--- react-router-6/src/App.jsx
+++ react-router-6/src/App.jsx
@@ -4,6 +4,7 @@ import Error from "./pages/Error";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProduct";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
+          <Route path="products/:productId" element={<SingleProduct />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
--- react-router-6/src/pages/SingleProduct.jsx
+++ react-router-6/src/pages/SingleProduct.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import products from "../data";
+
 const SingleProduct = () => {
-  return <div>SingleProduct</div>;
+  const { productId } = useParams();
+
+  const [product, setProduct] = useState({ id: "", name: "", image: "" });
+  const [notExists, setNotExists] = useState(false);
+
+  useEffect(() => {
+    const product = products.find((product) => product.id === productId);
+    if (product) {
+      setNotExists(false);
+      setProduct(product);
+    } else {
+      setNotExists(true);
+    }
+  }, [productId]);
+
+  return (
+    <section className="section product">
+      {notExists ? (
+        <h1>No Product found</h1>
+      ) : (
+        <>
+          <img src={product.image} alt={product.name} />
+          <h5>{product.name}</h5>
+        </>
+      )}
+      <Link to="/products" className="btn">
+        view all products
+      </Link>
+    </section>
+  );
 };
 
 export default SingleProduct;
