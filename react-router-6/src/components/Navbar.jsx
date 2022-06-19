45359f1eaba6bf294d5a2b0104059b0f3248777c
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-06-19 11:12:34 +0100

    changed to NavLink for the links in navbar

--- react-router-6/src/components/Navbar.jsx
+++ react-router-6/src/components/Navbar.jsx
@@ -1,11 +1,32 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/products">Products</Link>
+      <NavLink
+        to="/"
+        className={({ isActive }) => {
+          return isActive ? "link active" : "link";
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) => {
+          return isActive ? "link active" : "link";
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/products"
+        className={({ isActive }) => {
+          return isActive ? "link active" : "link";
+        }}
+      >
+        Products
+      </NavLink>
     </nav>
   );
 };
